e95b6c77943478d6cce7f34f8885739a3f10c215
songhaibing-web <2379223499@qq.com>
2024-08-09 12:22:25 +0800

    删除菜品（涉及三张表，开启事务@Tranactional，for(类型 id ： ids，动态sql foreach，前端传1，2,3需要使用@RequestParam，BaseException 业务异常)）

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -7,8 +7,11 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 菜单管理
@@ -42,5 +45,17 @@ public class DishController {
         log.info("菜品分页查询：{}", dishPageQueryDTO);
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids) {
+        log.info("菜品的批量删除：{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
     }
 }
