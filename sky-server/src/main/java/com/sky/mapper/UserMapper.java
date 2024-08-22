aaa777b3927f37652d6c18d8a193dbe56a6a2ec0
Administrator <2379223499@qq.com>
2024-08-22 23:25:37 +0800

    订单支付功能（没有两个文件，功能没有实现）

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,4 +20,13 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+
+    /**
+     * 根据id获取信息
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
